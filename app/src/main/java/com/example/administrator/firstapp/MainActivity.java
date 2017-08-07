832c019c4b93b0262b26a829f4d66ad5196a4698
weifirst <wei@weisoft.com>
2017-08-07 12:42:26 +0800

    无须注释

--- app/src/main/java/com/example/administrator/firstapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/firstapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.administrator.firstapp;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +15,8 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -23,8 +28,13 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import static com.example.administrator.firstapp.Main3Activity.transparencyBar;
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +46,35 @@ public class MainActivity extends AppCompatActivity {
 
     private FrameLayout body_layout;
     private ContentView content;
+
+    @TargetApi(19)
+    public static void transparencyBar(Activity activity){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window =activity.getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;        // a|=b的意思就是把a和b按位或然后赋值给a   按位或的意思就是先把a和b都换成2进制，然后用或操作，相当于a=a|b
+        }
+        else{
+            winParams.flags &= ~bits;        //&是位运算里面，与运算  a&=b相当于 a = a&b  ~非运算符
+        }
+        win.setAttributes(winParams);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +90,55 @@ public class MainActivity extends AppCompatActivity {
         body_layout = (FrameLayout) findViewById(R.id.body_layout);
 
         // 初始化一个显示各个点的viewGroup
-        content = new ContentView(this);
+        content = new ContentView(this,"12345",new Drawl.GestureCallBack() {
 
+            @Override
+            public void checkedSuccess() {
+                Toast.makeText(MainActivity.this,"校验成功", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void checkedFail() {
+                Toast.makeText(MainActivity.this,"校验失败", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+       // Intent intent = new Intent(this,Main3Activity.class);
+       // startActivity(intent);
         //设置手势解锁显示到哪个布局里面
         content.setParentView(body_layout);
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {            //系统版本大于19
+            setTranslucentStatus(true);
+        }
+        transparencyBar(this);
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        //tintManager.setStatusBarTintResource(R.color.title_green);
+        tintManager.setStatusBarTintResource(R.drawable.sky2);
+        Class clazz = this.getWindow().getClass();
+        try {
+            int darkModeFlag = 0;
+            Class layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            if(true){
+                extraFlagField.invoke(this.getWindow(),darkModeFlag,darkModeFlag);//状态栏透明且黑色字体
+            }
+            else{
+                extraFlagField.invoke(this.getWindow(), 0, darkModeFlag);//清除黑色字体
+            }
+        }
+        catch (Exception e){
+        }
     }
 
     @Override
@@ -141,6 +221,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private long exitTime = 0;
+    private long aaa = 1;//test git
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
