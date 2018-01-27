9443c5354c68660475526a8add937dca6737bd22
weifirst <wei@weisoft.com>
2018-01-27 23:17:15 +0800

    无须注释

--- app/src/main/java/com/example/administrator/firstapp/ListWnd.java
+++ app/src/main/java/com/example/administrator/firstapp/ListWnd.java
@@ -1,42 +1,33 @@
 package com.example.administrator.firstapp;
 
+
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.util.ArrayMap;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class ListWnd extends AppCompatActivity {
+public class ListWnd extends Activity implements AdapterView.OnItemClickListener{
     private ListView listView;
+    Map<Long,Integer> mymap = new ArrayMap<>();
 
-    public void Click(View v)
-    {
-          switch (v.getId()){
-          case R.id.btnSave:
-          {
-
-                return;
-
-          }
-
-        }
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         listView = new ListView(this);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,getData()));
+        listView.setOnItemClickListener(this);
         setContentView(listView);
     }
 
@@ -45,13 +36,16 @@ public class ListWnd extends AppCompatActivity {
 
         SQLiteDatabase db = openOrCreateDatabase("p2p.db", Context.MODE_PRIVATE, null);
         Cursor c = db.rawQuery("SELECT * FROM Invest",null);
+        Long i=new Long(0);
         while (c.moveToNext()) {
-            int _id = c.getInt(c.getColumnIndex("_id"));
+            Integer id = c.getInt(c.getColumnIndex("_id"));
             String riqi = c.getString(c.getColumnIndex("DaoqiDate"));
             int nJinE = c.getInt(c.getColumnIndex("JinE"));
             String PingtaiName = c.getString(c.getColumnIndex("PingTaiName"));
             String msg=String.format( "%s % 5d %s", riqi,nJinE,PingtaiName);
             data.add(msg);
+            mymap.put(i,id);
+            i++;
         }
         c.close();
         //关闭当前数据库
@@ -59,4 +53,17 @@ public class ListWnd extends AppCompatActivity {
 
         return data;
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //通过view获取其内部的组件，进而进行操作
+//        String text = (String) ((TextView)view.findViewById(R.id.text)).getText();
+        //大多数情况下，position和id相同，并且都从0开始
+  //      String showText = "点击第" + position + "项，文本内容为：" + text + "，ID为：" + id;
+        Long iid = new Long(id);
+        String showText = "ID为"+id+"  数据库ID为"+mymap.get(iid);
+        Toast.makeText(this, showText, Toast.LENGTH_LONG).show();
+      //  ListView.
+    }
+
 }
